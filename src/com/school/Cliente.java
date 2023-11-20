ac2709fa6fd297d5b9d07d2d1721f9ca064b6d87
Carlos Domingues <143935068+carlosdomingues1974@users.noreply.github.com>
2023-11-20 14:08:58 +0000

    Adicionar atributos à classe Cliente

--- src/com/school/Cliente.java
+++ src/com/school/Cliente.java
@@ -1,4 +1,10 @@
 package com.school;
 
+/**
+ * Classe para criar e gerir objetos do tipo Cliente
+ */
 public class Cliente {
+    //Atributos
+    private String nome; //nome do cliente
+    private String apelido; //apelido do cliente
 }
