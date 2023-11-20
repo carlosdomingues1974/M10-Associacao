933d3a9042c22b80ad22d6246b525fe7df1f8dbf
Carlos Domingues <143935068+carlosdomingues1974@users.noreply.github.com>
2023-11-20 14:12:54 +0000

    Adicionar os métodos construtores à classe Cliente

--- src/com/school/Cliente.java
+++ src/com/school/Cliente.java
@@ -7,4 +7,24 @@ public class Cliente {
     //Atributos
     private String nome; //nome do cliente
     private String apelido; //apelido do cliente
+
+    //Métodos construtores
+
+    /**
+     * Construtor default
+     * Só deve ser utilizado quando não existir mais nenhum
+     */
+    public Cliente() {
+    }
+
+    /**
+     * Construtor completo
+     * Permite permite criar objetos com todos os atributos
+     * @param nome recebe uma String com o nome do cliente
+     * @param apelido recebe uma String com o apelido do cliente
+     */
+    public Cliente(String nome, String apelido) {
+        this.nome = nome;
+        this.apelido = apelido;
+    }
 }
