4d1c4271cb062f43117fdee92c6cbc45a1d77bc1
Carlos Domingues <143935068+carlosdomingues1974@users.noreply.github.com>
2023-11-20 14:25:13 +0000

    Acrescentar os atributos à classe ContaBancaria

--- src/com/school/ContaBancaria.java
+++ src/com/school/ContaBancaria.java
@@ -4,4 +4,8 @@ package com.school;
  * Classe para criar e gerir objetos do tipo ContaBancaria
  */
 public class ContaBancaria {
+    //Atributos
+    private String nib; //o número do nib da conta bancária
+    private Cliente titular;   //o titular da conta bancária
+    private double saldo;   //o saldo da conta bancária
 }
