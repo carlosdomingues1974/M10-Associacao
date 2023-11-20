e02a5bc4c8fa7a48c4ed30d68af3146cc655b2da
Carlos Domingues <143935068+carlosdomingues1974@users.noreply.github.com>
2023-11-20 14:35:04 +0000

    Acrescentar os métodos construtores à classe ContaBancaria

--- src/com/school/ContaBancaria.java
+++ src/com/school/ContaBancaria.java
@@ -8,4 +8,26 @@ public class ContaBancaria {
     private String nib; //o número do nib da conta bancária
     private Cliente titular;   //o titular da conta bancária
     private double saldo;   //o saldo da conta bancária
+
+    //Métodos construtores
+
+    /**
+     * Construtor default
+     * Só deve ser utilizado quando não existir mais nenhum
+     */
+    public ContaBancaria() {
+    }
+
+    /**
+     * Construtor completo
+     * Permite permite criar objetos com todos os atributos
+     * @param nib recebe uma String com o número da conta bancária
+     * @param titular recebe um objeto do tipo Cliente com o titular da conta bancária
+     * @param saldo recebe uma valor com o saldo da conta bancária
+     */
+    public ContaBancaria(String nib, Cliente titular, double saldo) {
+        this.nib = nib;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
 }
